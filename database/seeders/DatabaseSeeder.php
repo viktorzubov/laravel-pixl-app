b9125dfe8bf9bb84d2920df15f085aae3be9cc7c
viktor <13069159+viktorzubov@users.noreply.github.com>
2026-06-09 19:43:45 +0300

    Seed the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Follow;
+use App\Models\Like;
+use App\Models\Post;
+use App\Models\Profile;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +18,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $profiles = Profile::factory()->count(20)->create();
 
-        User::factory()->create([
-            // 'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($profiles as $profile) {
+            Post::class::factory()->count(5)->create([
+                'profile_id' => $profile->id,
+            ]);
+        }
+
+        $posts = Post::all();
+
+        foreach ($profiles as $profile) {
+            $toFollow = $profiles->except($profile->id)->random(rand(3, 7));
+
+            foreach ($toFollow as $target) {
+                Follow::createFollow($profile, $target);
+            }
+        }
+
+        foreach ($profiles as $profile) {
+            $toLike = $posts->where('profile_id', '!=', $profile->id)->random(rand(10, 20));
+
+            foreach ($toLike as $post) {
+                Like::createLike($profile, $post);
+            }
+        }
+
+        foreach ($profiles as $profile) {
+            $toRepost = $posts->where('profile_id', '!=', $profile->id)->random(rand(2, 5));
+
+            foreach ($toRepost as $post) {
+                Post::repost($profile, $post, rand(0, 1) ? null : 'Great post!');
+            }
+        }
+
+        for ($i = 0; $i < rand(20, 30); $i++) {
+            $parentPost = $posts->random();
+            $replier = $profiles->where('id', '!=', $parentPost->profile_id)->random();
+
+            Post::factory()->reply($parentPost)->create([
+                'profile_id' => $replier->id,
+            ]);
+        }
+
     }
 }
