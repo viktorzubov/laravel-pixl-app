7bd3417ed3e12ac57083dcf7d49c999c30d5b1eb
Victor Zubov <13069159+viktorzubov@users.noreply.github.com>
2026-06-10 15:30:24 +0300

    Add authentication routes for login and logout functionality (#9)

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,29 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dev/login', function () {
+    $user = User::inRandomOrder()->first();
+
+    Auth::login($user);
+    request()->session()->regenerate();
+
+    return redirect()->intended(route('profiles.show', $user->profile));
+});
+
+Route::get('/dev/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+
+    return redirect()->intended('/feed');
 });
 
 Route::get('/{profile:handle}', [ProfileController::class, 'show'])->name('profiles.show');
